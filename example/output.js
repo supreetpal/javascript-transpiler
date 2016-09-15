bd7fc0803da3cffbdd875c079d9309372719275b
Supreet Pal Singh <supreetpal@gmail.com>
2016-09-15 11:21:12 +0530

    Push basic variable transpiling.

---
+++ example/output.js
@@ -0,0 +1,1 @@
+variable iis equal to2
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import * as babylon from "babylon";
+import traverse from "babel-traverse";
+import fs from "fs";
+
+let inputFile = process.argv[2],
+    outputFile = process.argv[3];
+
+const code = fs.readFileSync(inputFile, "utf-8");
+
+const ast = babylon.parse(code);
+
+let sourceString = "";
+
+traverse(ast, {
+    enter(path) {
+        if (path.node.type === "VariableDeclarator") {
+            sourceString += 'variable ' + path.node.id.name; 
+            sourceString += ' is equal to ';
+            sourceString += path.node.init.value + '';
+        }
+    },
+
+    exit(path) {
+        
+    }
+});
+
+fs.writeFileSync(outputFile, sourceString, "utf-8");
